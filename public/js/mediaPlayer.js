a09e8d665d29a3c6490f876b3c4293d526644518
FyWolf <alanpoux@gmail.com>
2024-03-29 13:06:27 +0100

    small mediaplayer update

--- public/js/mediaPlayer.js
+++ public/js/mediaPlayer.js
@@ -1,18 +1,31 @@
 const mediaPlayer = document.getElementById("audio-player");
 const pauseButton = document.getElementById("MPPlayBtn");
 const songTitle = document.getElementById("MPSongTitle");
-let isSongPlaying = true;
+const jsmediatags = window.jsmediatags;
+let isSongPlaying = false;
 
 pauseButton.addEventListener('click', () => {
     playPause();
 });
 
-function playPause(){
-    if(isSongPlaying) {
+function playPause() {
+    if (isSongPlaying) {
         mediaPlayer.pause();
         isSongPlaying = false;
     } else {
         mediaPlayer.play();
+        readMeta();
         isSongPlaying = true;
     }
+}
+
+function readMeta() {
+    jsmediatags.read(mediaPlayer.src, {
+        onSuccess: function (tag) {
+            songTitle.innerText = tag.tags.title;
+        },
+        onError: function (error) {
+            console.log(error);
+        }
+    });
 }
